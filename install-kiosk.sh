8b704f2372430550d3d805c763ecccc4b52f3703
Yash <138782151+yash-wst@users.noreply.github.com>
2025-09-12 18:36:51 +0530

    Update install-kiosk.sh

--- install-kiosk.sh
+++ install-kiosk.sh
@@ -3,4 +3,4 @@ set -e
 sudo apt update
 sudo apt install -y libfuse2 || sudo apt install -y fuse
 wget -O LinuxExamKiosk1.0.0.AppImage -v https://download.weshinetech.in/examkiosk/LinuxExamKiosk1.0.0
-sudo chmod a+x LinuxExamKiosk1.0.0.AppImage
+sudo chmod a+x LinuxExamKiosk1.0.0
